5f288a2bfd25b8320daecf68ec5e0a0b57d62c81
jawad Bin Azam <abidibnazam@gmail.com>
2023-07-26 16:38:37 +0600

    frequent element in an array done

---
+++ frequentElement.js
@@ -0,0 +1,29 @@
+/*
+todo: Task 3: Write a JavaScript program to find the most frequent element in an array and return it.
+ */
+
+const mostFrequentElement = (arr) => {
+  const freqObj = {};
+  let maxCount = 1;
+  let maxElement = arr[0];
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    const element = arr[i];
+    // console.log(element);
+    if (freqObj[element]) {
+      freqObj[element]++;
+    } else {
+      freqObj[element] = 1;
+    }
+    if (freqObj[element] > maxCount) {
+      maxElement = element;
+      maxCount = freqObj[element];
+    }
+  }
+  console.log(freqObj);
+  return `Frequent Element is ${maxElement} and Count ${maxCount}`;
+};
+
+const inputArray = [3, 5, 2, 5, 3, 3, 1, 4];
+const result = mostFrequentElement(inputArray);
+console.log(result);
--- task.md
+++ task.md
@@ -8,7 +8,7 @@ Example Input: "hello world" Example Output: "dlrow olleh"
 
 Example Input: [2, -5, 10, -3, 7] Example Output: 19
 
-Task 3: Write a JavaScript program to find the most frequent element in an array and return it.
+<!--todo: Task 3: Write a JavaScript program to find the most frequent element in an array and return it. -->
 
 Example Input: [3, 5, 2, 5, 3, 3, 1, 4, 5] Example Output: 3
 
